f1eed8f24e9d7243f334a5c751658bde9c4a9143
anton <anton.dontsovgs@mail.ru>
2023-06-01 12:30:22 +0300

    fix bag

--- bewise_second/index/utils.py
+++ bewise_second/index/utils.py
@@ -11,8 +11,8 @@ def convert_to_mp3(audio_file):
     audio_name = '.'.join(audio_file.split('.')[:-1])
     print(audio_name)
 
-    src = os.path.join(BASE_DIR, rf"media\{audio_name}.wav") # src to dst
-    dst = os.path.join(BASE_DIR, rf"media\{audio_name}.mp3") # wav to mp3
+    src = os.path.join(BASE_DIR, rf"media/{audio_name}.wav") # src to dst
+    dst = os.path.join(BASE_DIR, rf"media/{audio_name}.mp3") # wav to mp3
 
     sound = AudioSegment.from_mp3(src)
     sound.export(dst, format="wav")
